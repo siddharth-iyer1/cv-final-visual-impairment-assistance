76d83af26402cebb529f35d0e1e8c653f7c62594
Siddharth <siddharth.iyer123@gmail.com>
2024-04-28 18:56:13 -0500

    file reorg + generated pics

---
+++ camera calibration/checkboard calibration/generate_calibration_images.py
@@ -0,0 +1,29 @@
+import cv2
+
+cap1 = cv2.VideoCapture(0)
+cap2 = cv2.VideoCapture(1)
+
+num_pics = 0
+
+while cap1.isOpened() and cap2.isOpened():
+
+    ret1, frame1 = cap1.read()
+    ret2, frame2 = cap2.read()
+
+    k = cv2.waitKey(5)
+
+    if k == 27:
+        break
+    elif k == ord('s'):
+        num_pics += 1
+        cv2.imwrite(f'/Users/siddharthiyer/Documents/GitHub/cv-final-visual-impairment-assistance/camera calibration/checkboard calibration/images/left cam/' + str(num_pics) + '.png', frame2)
+        cv2.imwrite(f'/Users/siddharthiyer/Documents/GitHub/cv-final-visual-impairment-assistance/camera calibration/checkboard calibration/images/right cam/' + str(num_pics) + '.png', frame1)
+        print("images saved")
+
+    cv2.imshow('frame1', frame1)
+    cv2.imshow('frame2', frame2)
+
+cap1.release()
+cap2.release()
+
+cv2.destroyAllWindows()
